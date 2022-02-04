fc17eb06b433cb846c67f101cbed9c47c666bc0e
Mufaddal Hamid <mufaddalhamidofficial@example.com>
2022-02-04 22:39:08 +0530

    04-02

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 const bootcamps = require('./routes/bootcamps');
 
+//Load env vars
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
 const app = express();
+
+if (process.env.NODE_ENV == 'development') app.use(morgan('dev'));
 
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log(`Unhandled Rejection: ${err.message}`.red.underline.bold);
+  server.close(() => process.exit(1));
+});
